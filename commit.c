063a92f4d9c4fc1cadd39e6d07aa840a8103005c
prathikshaholla <prathiksha1608@gmail.com>
2026-04-20 21:31:34 +0530

    Phase 4: implement commit_create

--- commit.c
+++ commit.c
@@ -194,8 +194,36 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
+    if (!message || !message[0]) return -1;
+
+    Commit commit;
+    memset(&commit, 0, sizeof(commit));
+
+    if (tree_from_index(&commit.tree) != 0) return -1;
+
+    if (head_read(&commit.parent) == 0) {
+        commit.has_parent = 1;
+    } else {
+        commit.has_parent = 0;
+    }
+
+    snprintf(commit.author, sizeof(commit.author), "%s", pes_author());
+    commit.timestamp = (uint64_t)time(NULL);
+    snprintf(commit.message, sizeof(commit.message), "%s", message);
+
+    void *serialized = NULL;
+    size_t serialized_len = 0;
+    if (commit_serialize(&commit, &serialized, &serialized_len) != 0) return -1;
+
+    ObjectID new_commit;
+    if (object_write(OBJ_COMMIT, serialized, serialized_len, &new_commit) != 0) {
+        free(serialized);
+        return -1;
+    }
+    free(serialized);
+
+    if (head_update(&new_commit) != 0) return -1;
+
+    *commit_id_out = new_commit;
+    return 0;
 }
